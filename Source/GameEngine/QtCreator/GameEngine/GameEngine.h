0646bba233a30250e9d0e361c2eb169f84e80098
Bear <tbearcuf@gmail.com>
2018-06-09 13:19:19 -0700

    created project

---
+++ Source/GameEngine/QtCreator/GameEngine/GameEngine.h
@@ -0,0 +1,13 @@
+#ifndef GAMEENGINE_H
+#define GAMEENGINE_H
+
+#include "gameengine_global.h"
+
+class GAMEENGINESHARED_EXPORT GameEngine
+{
+
+public:
+    GameEngine();
+};
+
+#endif // GAMEENGINE_H
