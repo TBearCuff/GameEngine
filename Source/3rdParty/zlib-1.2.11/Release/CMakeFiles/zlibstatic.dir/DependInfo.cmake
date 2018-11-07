5953d4a9bd3775b09ded36caa7ac4f3405d33a12
Bear <tbearcuff@gmail.com>
2018-11-07 12:25:14 -0800

    Added zlib library files.

---
+++ Source/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/adler32.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/adler32.o"
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/compress.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/compress.o"
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/crc32.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/crc32.o"
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/deflate.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/deflate.o"
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/gzclose.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/gzclose.o"
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/gzlib.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/gzlib.o"
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/gzread.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/gzread.o"
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/gzwrite.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/gzwrite.o"
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/infback.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/infback.o"
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/inffast.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/inffast.o"
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/inflate.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/inflate.o"
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/inftrees.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/inftrees.o"
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/trees.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/trees.o"
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/uncompr.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/uncompr.o"
+  "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/zutil.c" "/home/troy/UBG/Source/UBGEngine/3rdParty/zlib-1.2.11/Release/CMakeFiles/zlibstatic.dir/zutil.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_LARGEFILE64_SOURCE=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
