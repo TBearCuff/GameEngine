a0753b1e9d21a7f16ca0d8efd20fee998ece4cf4
Bear <tbearcuff@gmail.com>
2018-11-14 09:06:08 -0800

    Resource name (identifier) is now all lower case.

--- Source/ResourceCache/ResCache.cpp
+++ Source/ResourceCache/ResCache.cpp
@@ -12,8 +12,7 @@
 //
 Resource::Resource(const QString &name)
 {
-    m_name=name;
-//    std::transform(m_name.begin(), m_name.end(), m_name.begin(), (int(*)(int)) std::tolower);
+    m_name=name.toLower();
 }
 
 //=================================================================
